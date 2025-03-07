a71b0d89a3d5468d0ea1ad7968f95a1ca6a3b803
barronh <barronh@gmail.com>
2025-03-07 15:32:14 -0500

    removing old version

--- tempodash/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['cfg', 'defn', 'get', 'pair', 'plots', 'word', 'util']
-
-from . import cfg
-from . import defn
-from . import get
-from . import pair
-from . import plots
-from . import word
-from . import util
